71c72a9b585bb0a929a36ecb7ff21bc787648fa1
brittanymae01 <brittanymae01>
2019-12-06 08:26:58 -0800

    rendered data

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card'
 import axios from 'axios'
 
 class App extends React.Component {
@@ -14,26 +14,25 @@ class App extends React.Component {
   componentDidMount() {
     axios.get('http://localhost:5000/api/players').then(response => {
       console.log(response)
+      this.setState({
+        data: response.data
+      })
     })
   }
 
   render() {
+    console.log(this.state)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
+        <div className='cards'>
+          {this.state.data.map(person => (
+            <Card
+              key={person.id}
+              name={person.name}
+              country={person.country}
+              searches={person.searches} />
+          ))}
+        </div>
       </div>
     );
   }
---
+++ client/src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = props => {
+    return (
+        <div className='card'>
+            <h2>Name: {props.name}</h2>
+            <span></span>
+            <p>Country: {props.country}</p>
+            <p>Searches: {props.searches}</p>
+        </div>
+    )
+};
+
+export default Card;
